a3e37320cc5f6e3688619139bf533f9c514afd4b
Maariya Qureshi <maariyaqureshi1@gmail.com>
2025-07-10 07:17:54 -0400

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Maariya Qureshi on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
